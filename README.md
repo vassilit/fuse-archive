21bc15d5ac3b9aa33958a3a6d5c955ae756e5164
François Degros <fdegros@chromium.org>
2026-08-04 15:06:30 +1000

    Start development version 1.25

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 title: fuse-archive
 section: 1
 header: User Manual
-footer: fuse-archive 1.24
+footer: fuse-archive 1.25
 date: August 2026
 ---
 
--- fuse-archive.cc
+++ fuse-archive.cc
@@ -73,7 +73,7 @@
 #include "lib/util.h"
 
 #define PROGRAM_NAME "fuse-archive"
-#define PROGRAM_VERSION "1.24"
+#define PROGRAM_VERSION "1.25"
 
 // ---- Compile-time Configuration
 
